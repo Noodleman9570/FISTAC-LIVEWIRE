e5f03211228ea8aa7aa7e105366276f713957396
Noodleman9570 <kevinsaavedra55@gmail.com>
2024-01-21 14:23:54 -0400

    proceso de verificar si existe contribuyente:

--- app/Livewire/AsigTimElec/Index.php
+++ app/Livewire/AsigTimElec/Index.php
@@ -1,11 +1,29 @@
 <?php
 
 namespace App\Livewire\AsigTimElec;
-
+use App\Livewire\Contribuyentes\Forms\ContribuyenteCreateForm;
+use App\Models\Contribuyente;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public ContribuyenteCreateForm $contribuyenteCreate;
+    public $contribuyente;
+
+    public function contribSearch()
+    {
+        $contribuyente = Contribuyente::where('cedula', $this->contribuyenteCreate->cedula)->first();
+        if ($contribuyente) {
+            $this->contribuyenteCreate->prefijo = $contribuyente->prefijo;
+            $this->contribuyenteCreate->nombre = $contribuyente->nombre;
+            $this->contribuyenteCreate->apellido = $contribuyente->apellido;
+            $this->contribuyenteCreate->direccion = $contribuyente->direccion;
+            $this->contribuyenteCreate->telefono = $contribuyente->telefono;
+        }else{
+            return null;
+        }
+    }
+
     public function render()
     {
         return view('livewire.AsigTimElec.index');
--- resources/views/livewire/AsigTimElec/index.blade.php
+++ resources/views/livewire/AsigTimElec/index.blade.php
@@ -5,41 +5,125 @@
         <div class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
           <div class="border-black/12.5 rounded-t-2xl border-b-0 border-solid p-6 pb-0">
             <div class="flex items-center">
-              <p class="mb-0 dark:text-white/80">Edit Profile</p>
+              <p class="mb-0 dark:text-white/80">Asignacin de Timbre Electrónico</p>
               <button type="button" class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-blue-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Settings</button>
             </div>
           </div>
           <div class="flex-auto p-6">
-            <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm">User Information</p>
+            <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm mb-4">Datos del Contribuyente</p>
+            {{-- Formulario Contribuyente --}}
             <div class="flex flex-wrap -mx-3">
+              {{-- Cedula --}}
               <div class="w-full max-w-full px-3 shrink-0 md:w-6/12 md:flex-0">
                 <div class="mb-4">
-                  <label for="username" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Username</label>
-                  <input type="text" name="username" value="lucky.jesse" class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+
+                  <x-input-label>
+                    Cedula:
+                </x-input-label>
+                <div class="flex mb-4">
+                    <span class="mr-2">
+                        <x-select class="w-20" wire:model="contribuyenteCreate.prefijo">
+                            <option value="" disabled>
+                                ...
+                            </option>
+                            <option value="V">V</option>
+                            <option value="E">E</option>
+                            <option value="J">J</option>
+                            <option value="G">G</option>
+                        </x-select>
+                    </span>
+                    
+                    <div class="column w-full">
+                        <x-text-input class="w-full" wire:model="contribuyenteCreate.cedula" wire:keydown='contribSearch'></x-text-input>
+                    </div>
+                    
+                </div>
+                  
                 </div>
               </div>
+              {{-- Nombre input --}}
+              <div class="w-full max-w-full px-3 shrink-0 md:w-1/4 md:flex-0">
+                <div class="mb-4">
+                  <span class="w-full mr-2">
+                    <x-input-label>
+                        Nombre:
+                    </x-input-label>
+
+                    <x-text-input class="w-full" wire:model="contribuyenteCreate.nombre"></x-text-input>
+                    
+                  </span>
+                </div>
+              </div>
+              {{-- Segundo nombre input --}}
+              <div class="w-full max-w-full px-3 shrink-0 md:w-1/4 md:flex-0">
+                <div class="mb-4">
+                  <span class="w-full mr-2">
+                    <x-input-label>
+                        Segundo nombre:
+                    </x-input-label>
+
+                    <x-text-input class="w-full" wire:model="contribuyenteCreate.nombre2nd"></x-text-input>
+                    
+                  </span>
+                </div>
+              </div>
+              {{-- Telefono input --}}
               <div class="w-full max-w-full px-3 shrink-0 md:w-6/12 md:flex-0">
                 <div class="mb-4">
-                  <label for="email" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Email address</label>
-                  <input type="email" name="email" value="jesse@example.com" class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                  <span class="w-full mr-2">
+                    <x-input-label>
+                        Telefono:
+                    </x-input-label>
+
+                    <x-text-input class="w-full" wire:model="contribuyenteCreate.telefono"></x-text-input>
+                    
+                </span>
                 </div>
               </div>
-              <div class="w-full max-w-full px-3 shrink-0 md:w-6/12 md:flex-0">
+              {{-- Apellido input --}}
+              <div class="w-full max-w-full px-3 shrink-0 md:w-1/4 md:flex-0">
                 <div class="mb-4">
-                  <label for="first name" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">First name</label>
-                  <input type="text" name="first name" value="Jesse" class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                  <span class="w-full mr-2">
+                    <x-input-label>
+                        Apellido:
+                    </x-input-label>
+
+                    <x-text-input class="w-full" wire:model="contribuyenteCreate.apellido"></x-text-input>
+                    
+                  </span>
                 </div>
               </div>
-              <div class="w-full max-w-full px-3 shrink-0 md:w-6/12 md:flex-0">
+              {{-- Segundo apellido input --}}
+              <div class="w-full max-w-full px-3 shrink-0 md:w-1/4 md:flex-0">
                 <div class="mb-4">
-                  <label for="last name" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Last name</label>
-                  <input type="text" name="last name" value="Lucky" class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                  <span class="w-full mr-2">
+                    <x-input-label>
+                        Segundo apellido:
+                    </x-input-label>
+
+                    <x-text-input class="w-full" wire:model="contribuyenteCreate.apellido2nd"></x-text-input>
+                    
+                  </span>
                 </div>
               </div>
+              {{-- Direccion --}}
+              <div class="w-full max-w-full px-3 shrink-0 md:flex-0">
+                <div class="mb-4">
+                  <x-input-label>
+                    Direccion:
+                  </x-input-label>
+
+                  <x-textarea class="w-full" wire:model="contribuyenteCreate.direccion"></x-textarea>
+                  <x-input-error-jet for="contribuyenteCreate.direccion" />
+                </div>
+              </div>
+
             </div>
+
             <hr class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
             
-            <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm">Contact Information</p>
+            <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm mb-3">Datos del tramite</p>
+            {{-- Datos del tramite --}}
             <div class="flex flex-wrap -mx-3">
               <div class="w-full max-w-full px-3 shrink-0 md:w-full md:flex-0">
                 <div class="mb-4">
@@ -66,6 +150,7 @@
                 </div>
               </div>
             </div>
+
             <hr class="h-px mx-0 my-4 bg-transparent border-0 opacity-25 bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent " />
 
             <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm">About me</p>
@@ -82,12 +167,12 @@
       </div>
       <div class="w-full max-w-full px-3 mt-6 shrink-0 md:w-4/12 md:flex-0 md:mt-0">
         <div class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
-          <img class="w-full rounded-t-2xl" src="../assets/img/bg-profile.jpg" alt="profile cover image">
+          <img class="w-full rounded-t-2xl" src="{{ asset('img/user/bg-profile.jpg') }}" alt="profile cover image">
           <div class="flex flex-wrap justify-center -mx-3">
             <div class="w-4/12 max-w-full px-3 flex-0 ">
               <div class="mb-6 -mt-6 lg:mb-0 lg:-mt-16">
                 <a href="javascript:;">
-                  <img class="h-auto max-w-full border-2 border-white border-solid rounded-circle" src="../assets/img/team-2.jpg" alt="profile image">
+                  <img class="h-auto max-w-full border-2 border-white border-solid rounded-circle" src="{{ asset('/img/user/team-2.jpg') }}" alt="profile image">
                 </a>
               </div>
             </div>
