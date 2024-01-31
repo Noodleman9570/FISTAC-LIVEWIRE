f97566e930e39cdbc4452089065b9ae0573a5720
Kevin <kevinsaavedra55@gmail.com>
2024-01-31 10:59:35 -0400

    Se modificaron detallitos en el modulo usuarios

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
+
+    Volt::route('/', 'pages.auth.login')
+        ->name('login');
+
     Volt::route('register', 'pages.auth.register')
         ->name('register');
 
