04466d9f8ff6fc375716f127dd03754309262b93
Kevin <kevinsaavedra55@gmail.com>
2024-02-07 10:27:00 -0400

    push pop para ahora, push pop para despues

---
+++ app/Livewire/TimbresFiscales/CantidadTimbresChart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\TimbresFiscales;
+
+use Livewire\Component;
+
+class CantidadTimbresChart extends Component
+{
+
+    public function mount(){
+        
+    }
+
+    public function render()
+    {
+        return view('livewire.TimbresFiscales.cantidad-timbres-chart');
+    }
+}
--- resources/views/livewire/TimbresFiscales/index.blade.php
+++ resources/views/livewire/TimbresFiscales/index.blade.php
@@ -12,7 +12,7 @@
 
 
             <div class="w-full max-w-full px-3 xl:w-1/2 xl:flex-none">
-                <div class="flex flex-wrap gap-4 -mx-3 p-3">
+                <div class="flex flex-wrap gap-4  -mx-3 p-3">
 
                     {{-- Primera card --}}
                     <div class="w-full max-w-full dark:bg-[#111c44] pt-0 rounded-2xl md:flex-none">
@@ -114,150 +114,12 @@
 
 
         <div class="flex flex-wrap -mx-3">
-            <div class="w-full max-w-full px-3 mt-6 Smd:flex-none">
+            <div class="w-full max-w-full lg:w-7/12 px-3 mt-6 Smd:flex-none">
                 <livewire:TimbresFiscales.LastTimbresTable>
             </div>
-            {{-- <div class="w-full max-w-full px-3 mt-6 md:w-5/12 md:flex-none">
-                <div
-                    class="relative flex flex-col h-full min-w-0 mb-6 break-words bg-white border-0 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
-                    <div class="p-6 px-4 pb-0 mb-0 border-b-0 rounded-t-2xl">
-                        <div class="flex flex-wrap -mx-3">
-                            <div class="max-w-full px-3 md:w-1/2 md:flex-none">
-                                <h6 class="mb-0 dark:text-white">Your Transactions</h6>
-                            </div>
-                            <div
-                                class="flex items-center justify-end max-w-full px-3 dark:text-white/80 md:w-1/2 md:flex-none">
-                                <i class="mr-2 far fa-calendar-alt"></i>
-                                <small>23 - 30 March 2020</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex-auto p-4 pt-6">
-                        <h6 class="mb-4 text-xs font-bold leading-tight uppercase dark:text-white text-slate-500">Newest
-                        </h6>
-                        <ul class="flex flex-col pl-0 mb-0 rounded-lg">
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 rounded-t-inherit text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-red-600 border-transparent bg-transparent text-center align-middle font-bold uppercase text-red-600 transition-all hover:opacity-75"><i
-                                            class="fas fa-arrow-down text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">Netflix
-                                        </h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">27 March 2020, at 12:30
-                                            PM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="relative z-10 inline-block m-0 text-sm font-semibold leading-normal text-transparent bg-gradient-to-tl from-red-600 to-orange-600 bg-clip-text">
-                                        - $ 2,500</p>
-                                </div>
-                            </li>
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 border-t-0 rounded-b-inherit text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-emerald-500 border-transparent bg-transparent text-center align-middle font-bold uppercase text-emerald-500 transition-all hover:opacity-75"><i
-                                            class="fas fa-arrow-up text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">Apple
-                                        </h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">27 March 2020, at 04:30
-                                            AM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="relative z-10 inline-block m-0 text-sm font-semibold leading-normal text-transparent bg-gradient-to-tl from-green-600 to-lime-400 bg-clip-text">
-                                        + $ 2,000</p>
-                                </div>
-                            </li>
-                        </ul>
-                        <h6 class="my-4 text-xs font-bold leading-tight uppercase dark:text-white text-slate-500">
-                            Yesterday</h6>
-                        <ul class="flex flex-col pl-0 mb-0 rounded-lg">
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 rounded-t-inherit text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-emerald-500 border-transparent bg-transparent text-center align-middle font-bold uppercase text-emerald-500 transition-all hover:opacity-75"><i
-                                            class="fas fa-arrow-up text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">Stripe
-                                        </h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">26 March 2020, at 13:45
-                                            PM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="relative z-10 inline-block m-0 text-sm font-semibold leading-normal text-transparent bg-gradient-to-tl from-green-600 to-lime-400 bg-clip-text">
-                                        + $ 750</p>
-                                </div>
-                            </li>
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 border-t-0 text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-emerald-500 border-transparent bg-transparent text-center align-middle font-bold uppercase text-emerald-500 transition-all hover:opacity-75"><i
-                                            class="fas fa-arrow-up text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">HubSpot
-                                        </h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">26 March 2020, at 12:30
-                                            PM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="relative z-10 inline-block m-0 text-sm font-semibold leading-normal text-transparent bg-gradient-to-tl from-green-600 to-lime-400 bg-clip-text">
-                                        + $ 1,000</p>
-                                </div>
-                            </li>
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 border-t-0 text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-emerald-500 border-transparent bg-transparent text-center align-middle font-bold uppercase text-emerald-500 transition-all hover:opacity-75"><i
-                                            class="fas fa-arrow-up text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">Creative
-                                            Tim</h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">26 March 2020, at 08:30
-                                            AM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="relative z-10 items-center inline-block m-0 text-sm font-semibold leading-normal text-transparent bg-gradient-to-tl from-green-600 to-lime-400 bg-clip-text">
-                                        + $ 2,500</p>
-                                </div>
-                            </li>
-                            <li
-                                class="relative flex justify-between px-4 py-2 pl-0 mb-2 border-0 border-t-0 rounded-b-inherit text-inherit rounded-xl">
-                                <div class="flex items-center">
-                                    <button
-                                        class="leading-pro ease-in text-xs bg-150 w-6.5 h-6.5 p-1.2 rounded-3.5xl tracking-tight-rem bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-slate-700 border-transparent bg-transparent text-center align-middle font-bold uppercase text-slate-700 transition-all hover:opacity-75"><i
-                                            class="fas fa-exclamation text-3xs"></i></button>
-                                    <div class="flex flex-col">
-                                        <h6 class="mb-1 text-sm leading-normal dark:text-white text-slate-700">Webflow
-                                        </h6>
-                                        <span class="text-xs leading-tight dark:text-white/80">26 March 2020, at 05:00
-                                            AM</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col items-center justify-center">
-                                    <p
-                                        class="flex items-center m-0 text-sm font-semibold leading-normal text-slate-700">
-                                        Pending</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div> --}}
+            <div class="w-full max-w-full px-3 mt-6 md:w-5/12 md:flex-none">
+                <livewire:TimbresFiscales.CantidadTimbresChart>
+            </div>
         </div>
 
 
